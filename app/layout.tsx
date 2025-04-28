faeea9982429a81023119d2187a68e26369c4fb0
ABDUL-1000 <abdullatifgiwa2019@gmail.com>
2025-04-28 20:08:01 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +20,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+     
+ 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       className="bg-[#0C0C0C]"
       >
-        {children}
+         
+        
+          <Navbar />
+          <main className="lg:mx-[140px]">
+
+          {/* container to center the content */}
+         
+            {children}
+          </main>
+        
+        
+  
       </body>
+
+      
+
     </html>
   );
 }
